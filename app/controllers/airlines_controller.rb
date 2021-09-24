4c4bb1538037cb11e09dbf40bc7540b8598b3bc4
Erin Quinn <equinn125@gmail.com>
2021-09-24 10:22:27 -0600

    impl refactor in feature view

--- app/controllers/airlines_controller.rb
+++ app/controllers/airlines_controller.rb
@@ -1,6 +1,5 @@
 class AirlinesController < ApplicationController
   def show
     @airline = Airline.find(params[:id])
-    @passengers = Passenger.passengers_by_airline(@airline.id).all_adults
   end
 end
